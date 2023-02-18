20f0b59efba25d864880641cf62961056bc949d2
Sergey <sivak.93@bk.ru>
2023-02-18 22:24:59 +0500

    Ex1 new version

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -3,22 +3,32 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.Write("Введите число: ");
-string number = Console.ReadLine();
-int len = number.Length;
-
-if (len == 5)
+int InputInt(string message)                        
 {
-    if (number[0] == number[4] && number[1] == number[3])
-    {
-        Console.WriteLine($"Число {number} - это палиндром");
+    System.Console.Write($"{message} ");
+    int value;
+    bool isCorrect = int.TryParse(Console.ReadLine(), out value);
+    if(isCorrect)
+    { 
+        return value;
     }
-    else
-    {
-        Console.WriteLine($"{number} -это НЕ палиндром");
-    }
+    System.Console.WriteLine("Вы ввели не число!");
+    Environment.Exit(1);
+    return 0;
+}
+
+int number = InputInt("Введите пятизначное число: ");
+
+if (number < 10000 || number > 99999)
+{
+    Console.WriteLine($"Число {number} не подходит.Пожалуйста введите другое число!");
+}
+
+if ((number/10000 == number%10) && ((number/1000)%10 == (number%100)/10))
+{
+    Console.WriteLine($"Число {number} является полиндромом,его можно читать слева на право и справа на лево!");
 }
 else
 {
-    Console.WriteLine($"Число {number} - не является пятизначным");
+    System.Console.WriteLine($"Число {number} не является полиндромом.");
 }
